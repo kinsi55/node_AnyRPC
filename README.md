f5c8fae0fa8afaf274338117861c25b7be06e61e
Kinsi <kinsi55@users.noreply.github.com>
2025-07-13 19:46:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,118 @@
+# AnyRPC
+
+RPC over any wire(HTTP, WebSocket, Redis, TPC, Bluetooth, Serial, ...) using any transport(JSON, Messagepack, Protobuf, ...) - with TypeScript!
+
+Why every existing RPC implementation ships with a fixed, builtin Wire and Transport? I dont know!
+
+This aims to fix that, making for a simple, pluggable wrapper for handling RPC calls that could easily be implemented in other languages as well.
+
+## Examples
+
+Obviously these include absolutely no error handling whatsoever but you get the gist
+
+#### WebSocket (Bidirectional)
+```ts
+import AnyRPC, { type RPC } from "anyrpc";
+import uWebSockets from "uWebSockets.js"
+
+interface BackendMethod extends RPC {
+	name: "BackendMethod";
+	call: { baz: number };
+	return: number;
+}
+
+interface ServiceMethod extends RPC {
+	name: "ServiceMethod";
+	call: null;
+	return: number;
+}
+
+// Backend
+(() => {
+	const app = uWebSockets.App();
+	const rpc = new AnyRPC(null);
+	const websockets = new Map<uWebSockets.WebSocket<any>, AnyRPC>();
+
+	const textDecoder = new TextDecoder();
+
+	app.ws("/wsEndpoint", {
+		open(ws) {
+			/*
+				Subchannels override the method for sending out data, but use the same RPC
+				handlers of the parent
+			*/
+			websockets.set(ws, rpc.getSubChannel(x => ws.send(JSON.stringify(x))));
+		},
+		message(ws, message) {
+			websockets.get(ws).tryConsume(JSON.parse(textDecoder.decode(message)));
+		},
+		close(ws) {
+			websockets.delete(ws);
+		}
+	});
+
+	app.listen(3000, x => null);
+
+	rpc.setHandler<BackendMethod>("BackendMethod", data => {
+		return data.baz + 1;
+	});
+
+	setInterval(async () => {
+		for(const channel of websockets.values())
+			console.log("Response from Service:", await channel.call<ServiceMethod>("ServiceMethod"));
+	}, 5000);
+});
+
+// Service
+(() => {
+	const ws = new WebSocket("0.0.0.0:3000/wsEndpoint");
+	const rpc = new AnyRPC(x => ws.send(JSON.stringify(x)));
+
+	ws.onmessage = (event) => rpc.tryConsume(JSON.parse(event.data));
+
+	rpc.setHandler<ServiceMethod>("ServiceMethod", () => {
+		return 0xdeadbeef;
+	});
+
+	rpc.call<BackendMethod>("BackendMethod", {baz: 41}).then(x => console.log("Answer to everything:", x));
+});
+```
+
+
+#### HTTP (Unidirectional, Backend can only be called, not make calls itself)
+
+```ts
+import AnyRPC, { type RPC } from "anyrpc";
+import uWebSockets from "uWebSockets.js"
+
+interface FooBar extends RPC {
+	name: "foobar";
+	call: { baz: number };
+	return: number;
+}
+
+// Backend
+(() => {
+	const rpc = new AnyRPC(null);
+
+	uWebSockets.App().post("/RPC/:method", (res, req) => {
+		// For sake of simplicity, I'm using a url param here
+		rpc.tryConsume(JSON.parse(req.getQuery("call")), cb => res.end(JSON.stringify(cb)));
+	}).listen(3000, x => null);
+
+	rpc.setHandler<FooBar>("foobar", async(data) => {
+		return data.baz + 1;
+	});
+});
+
+// Client
+(() => {
+	const rpc = new AnyRPC(x => {
+		return fetch(`0.0.0.0:3000/RPC/${x.method}?` + new URLSearchParams({
+			call: JSON.stringify(x)
+		}))
+	});
+
+	rpc.call<FooBar>("foobar", {baz: 41}).then(x => console.log("Answer to everything:", x));
+});
+```
---
+++ src/anyrpc.ts
@@ -0,0 +1,155 @@
+export interface RPC {
+	name: string;
+	call: any;
+	return: any;
+}
+
+interface AnyRPCMessage {
+	anyRpcCallId: number;
+}
+
+interface WrappedCall extends AnyRPCMessage {
+	method: string;
+	message: any;
+}
+
+interface WrappedResponse extends AnyRPCMessage {
+	responseOk: boolean;
+	response: any;
+}
+
+type RPCHandler<T extends RPC> = (data: T["call"]) => T["return"] | Promise<T["return"]>;
+type MessageSender = (msg: WrappedCall | WrappedResponse) => Promise<any> | any;
+
+let msgNum = 1;
+
+const KV = function() {};
+KV.prototype = Object.create(null);
+
+const FIRE_AND_FORGET_CALLID = -1;
+const DATA_UNCONSUMED = Symbol("DATA_UNCONSUMED");
+
+class AnyRPC {
+	#sendMethod: MessageSender;
+
+	#responseHandlers = new Map() as Map<number, (x: WrappedResponse) => any>;
+	// @ts-ignore
+	#rpcHandlers: {[key: string]: RPCHandler<any>} = new KV();
+	#handlerForward: AnyRPC | undefined;
+
+	constructor(sendMethod: (msg: WrappedCall) => any) {
+		this.#sendMethod = sendMethod.bind(this) as MessageSender;
+	}
+
+	getSubChannel(sendMethod: (msg: WrappedCall) => any): AnyRPC {
+		const sub = new AnyRPC(sendMethod);
+
+		sub.#handlerForward = this;
+
+		return sub;
+	}
+
+	async #call<T extends RPC>(wrappedMsg: WrappedCall, timeoutMs: number) : Promise<T["return"]> {
+		if(wrappedMsg.anyRpcCallId === FIRE_AND_FORGET_CALLID)
+			return this.#sendMethod(wrappedMsg);
+
+		const p = new Promise((res, rej) => {
+			const timeout = setTimeout(() => callback({
+				response: "Request timed out",
+				responseOk: false,
+				anyRpcCallId: -1
+			}), timeoutMs);
+
+			const callback = (msg: WrappedResponse) => {
+				clearTimeout(timeout);
+
+				this.#responseHandlers.delete(msgNum);
+
+				if(!msg.responseOk)
+					return rej(new Error(msg.response));
+
+				res(msg.response);
+				return true;
+			};
+
+			this.#responseHandlers.set(msgNum, callback);
+		});
+
+		await this.#sendMethod(wrappedMsg);
+
+		return p as Promise<T["return"]>;
+	}
+
+	async call<T extends RPC>(def: T["name"], data: T["call"] = null, timeoutMs = 5e3) : Promise<T["return"]> {
+		return this.#call({
+			anyRpcCallId: msgNum++,
+			method: def,
+			message: data
+		}, timeoutMs);
+	}
+
+	async callWithoutResponse<T extends RPC>(def: T["name"], data: T["call"] = null, timeoutMs = 5e3) : Promise<void> {
+		return this.#call({
+			anyRpcCallId: FIRE_AND_FORGET_CALLID,
+			method: def,
+			message: data
+		}, timeoutMs);
+	}
+
+	setForward<T extends RPC>(def: T["name"], target: AnyRPC, timeoutMs = 5e3) {
+		if(this.#handlerForward)
+			throw new Error("Cannot add Forward to Sub-Channel");
+
+		this.#rpcHandlers[def] = (data) => {
+			return target.call<T>(def, data, timeoutMs);
+		};
+	}
+
+	setHandler<T extends RPC>(def: T["name"], handler: RPCHandler<T>) {
+		if(this.#handlerForward)
+			throw new Error("Cannot add Handler to Sub-Channel");
+
+		this.#rpcHandlers[def] = handler;
+	}
+
+	tryConsume(message: AnyRPCMessage, responseHandlerOverride?: MessageSender): typeof DATA_UNCONSUMED | boolean | Promise<boolean> {
+		if((message as WrappedCall).method) {
+			return this.#handleCall(message as WrappedCall, responseHandlerOverride ?? this.#sendMethod);
+		} else if((message as WrappedResponse).responseOk !== undefined) {
+			return this.#handleResponse(message as WrappedResponse);
+		}
+		return DATA_UNCONSUMED;
+	}
+
+	async #handleCall(call: WrappedCall, sender: MessageSender): Promise<boolean> {
+		const response: WrappedResponse = {
+			anyRpcCallId: call.anyRpcCallId,
+			responseOk: false,
+			response: "Method not found"
+		};
+
+		const handler = this.#rpcHandlers[call.message];
+
+		if(call.anyRpcCallId === FIRE_AND_FORGET_CALLID)
+			return true;
+
+		if(handler) {
+			try {
+				response.responseOk = true;
+				response.response = await handler(call.message);
+			} catch(ex) {
+				response.response = (ex as Error)?.message || ex
+			}
+		}
+
+		return sender(response);
+	}
+
+	#handleResponse(response: WrappedResponse): boolean {
+		return this.#responseHandlers.get(response.anyRpcCallId)?.(response) ?? false;
+	}
+}
+
+export default AnyRPC;
+
+export {DATA_UNCONSUMED};
